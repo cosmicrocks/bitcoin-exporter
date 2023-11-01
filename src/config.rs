1952da7f2d51ed36eb6dd11b4b0b5dc0532d34bf
Itamar Perez <itamarperez@gmail.com>
2023-11-01 13:26:26 -0700

    build container

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,6 @@
 use anyhow::{Context, Result};
 use serde::Deserialize;
+use std::env;
 use std::fs::File;
 
 fn default_host() -> String {
@@ -7,7 +8,7 @@ fn default_host() -> String {
 }
 
 fn default_bind() -> String {
-    "127.0.0.1:9898".to_owned()
+	"127.0.0.1:3000".to_owned()
 }
 
 /// Config file
@@ -17,21 +18,35 @@ pub struct Config {
 	#[serde(default = "default_host")]
 	pub host: String,
 	/// rpc user
+	#[serde(default)]
 	pub user: String,
 	/// rpc password
+	#[serde(default)]
 	pub password: String,
 	/// bind to addr:port
 	#[serde(default = "default_bind")]
-	pub bind: String
+	pub bind: String,
 }
 
 impl Config {
 	pub fn read(config: &str) -> Result<Config> {
-		// open configuration file
+		// Open configuration file
 		let file = File::open(&config).with_context(|| format!("Can't open {}", &config))?;
-		// deserialize configuration
-		let config: Config =
+		// Deserialize configuration
+		let mut config: Config =
 			serde_yaml::from_reader(file).with_context(|| format!("Can't read {}", &config))?;
+
+		// Check if config values are set in environment variables
+		if let Ok(host) = env::var("BITCOIN_RPC_HOST") {
+			config.host = "http://".to_owned() + &host + ":8332";
+		}
+		if let Ok(user) = env::var("BITCOIN_RPC_USER") {
+			config.user = user;
+		}
+		if let Ok(password) = env::var("BITCOIN_RPC_PASSWORD") {
+			config.password = password;
+		}
+
 		Ok(config)
 	}
 }
